8defb91b7e85379317dfe38de2e0c14c1ca9396f
Francisco Villegas <110942860+FV9201@users.noreply.github.com>
2023-02-05 00:35:39 -0300

    Finish the rock-paper-scissors game
    Add function "game()" - inside this function you will find all the game
    Add function "updateScore()" - update player and computer scores
    Add function "showRoundWinner()" - shows the winner of the round
    Add function "showGameWinner()" - shows the winner of the game

--- rock-paper-scissors-game.js
+++ rock-paper-scissors-game.js
@@ -1,39 +1,99 @@
-function getComputerChoice() {
-  let computerChoice = Math.floor(Math.random() * 3);
-  return choices[computerChoice];
-}
+function game() {
+  function getComputerChoice() {
+    return Math.floor(Math.random() * 3);
+  }
 
-function convertChoice(Choice) {
-  let i = 0;
-  let encontre = false;
-  while (i < choices.length && !encontre) {
-    if (Choice == choices[i]) {
-      Choice = i;
-      encontre = true;
+  function playRound() {
+    function convertChoice(choice) {
+      let i = 0;
+      let encontre = false;
+      while (i < choices.length && !encontre) {
+        if (choice == choices[i]) {
+          choice = i;
+          encontre = true;
+        } else {
+          i++;
+        }
+      }
+
+      return choice;
+    }
+
+    playerChoice = playerChoice.toLowerCase();
+    playerChoice = convertChoice(playerChoice);
+    if (playerChoice == computerChoice) {
+      return null;
+    } else if (
+      playerChoice - computerChoice == 1 ||
+      playerChoice - computerChoice == -(choices.length - 1)
+    ) {
+      return 1;
     } else {
-      i++;
+      return 0;
     }
   }
 
-  return Choice;
-}
-
-function playRound(playerChoice, computerChoice) {
-  playerChoice = playerChoice.toLowerCase();
-  playerChoice = convertChoice(playerChoice);
-  computerChoice = convertChoice(computerChoice);
-  if (playerChoice == computerChoice) {
-    return "Tie!";
-  } else if (
-    playerChoice - computerChoice == 1 ||
-    playerChoice - computerChoice == -(choices.length - 1)
-  ) {
-    return `You Win! ${choices[playerChoice]} beats ${choices[computerChoice]}`;
-  } else {
-    return `You Lose! ${choices[computerChoice]} beats ${choices[playerChoice]}`;
+  function updateScore() {
+    if (roundWinner === 1) {
+      playerScore++;
+    } else if (roundWinner === 0) {
+      computerScore++;
+    } else {
+      playerScore++;
+      computerScore++;
+    }
   }
+
+  function showRoundWinner() {
+    let message = [
+      `You Lose! ${choices[computerChoice]} beats ${choices[playerChoice]}`,
+      `You Win! ${choices[playerChoice]} beats ${choices[computerChoice]}`,
+    ];
+
+    if (roundWinner !== null) {
+      console.log(message[roundWinner]);
+    } else {
+      console.log("Tie!");
+    }
+  }
+
+  function showGameWinner() {
+    function returnGameWinner() {
+      return playerScore > computerScore
+        ? 1
+        : computerScore > playerScore
+        ? 0
+        : null;
+    }
+
+    let gameWinner = returnGameWinner();
+    let message = [
+      `You Lose the game... better luck next time!`,
+      `You Win the game, congratulations!`,
+    ];
+
+    if (gameWinner !== null) {
+      console.log(message[gameWinner]);
+    } else {
+      console.log("Nobody won the game, it's a tie!");
+    }
+  }
+
+  let choices = ["rock", "paper", "scissors"];
+  let playerScore = 0;
+  let computerScore = 0;
+  let roundWinner;
+  let playerChoice;
+  let computerChoice;
+
+  for (let i = 0; i < 5; i++) {
+    playerChoice = prompt("Enter your choice: Rock | Paper | Scissors");
+    computerChoice = getComputerChoice();
+    roundWinner = playRound();
+    updateScore();
+    showRoundWinner();
+  }
+  showGameWinner();
 }
 
-let choices = ["rock", "paper", "scissors"];
-
-console.log(playRound("rock", getComputerChoice()));
+game();
